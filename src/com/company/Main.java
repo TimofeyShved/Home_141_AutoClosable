a42a3145853104850c100d77604ce53e766a29f1
TimofeyShved <timofeya123@inbox.ru>
2023-11-29 00:26:23 +0600

    Auto close

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,45 @@
 package com.company;
 
+import java.io.*;
+
+
+/**
+ * В этом главном классе, примере создадим и покажем авто-закрытие методов
+ */
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+
+        //Создаём редар(читалку), но не всё так просто,
+        // ведь нужно обработать ошибку, того что он не найден
+        // и после закрыть.
+        // Поэтому такая портянка и так каждый раз, при работе с файлом,
+        // ведь он имплементет 2 интерфейса Readable и Closeable
+        Reader reader = null;
+        try{
+            reader = new FileReader(new File("my file"));
+            // Что-то делаем с файлом ...
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        // но можно сделать проще, теперь не нужно его закрывать,
+        // автозакрытие файла
+        try(Reader reader2 = new FileReader(new File("my file"))){
+            // Что-то делаем с файлом ...
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+    
 }
